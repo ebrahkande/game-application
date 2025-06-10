f573229ccd2b1521ff765ac5bbd709e58a1a8b94
ebrahkande <ebrahkande@gmail.com>
2025-06-09 21:28:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+const guessInput = document.getElementById('guessInput');
+const checkButton = document.getElementById('checkButton');
+const messageDisplay = document.getElementById('message');
